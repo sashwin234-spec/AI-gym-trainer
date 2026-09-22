831766f33dba1f2f5444e3a1e1075006458c78d7
Ashwin <sashwin234@gmail.com>
2026-09-22 16:28:42 +0530

    main.py

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ def main():
             key="exercise-analysis",
             mode=WebRtcMode.SENDRECV,
             video_processor_factory=VideoProcessorClass,
-            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+            rtc_configuration={"iceServers": [{"urls": ["stun:stun.cloudflare.com:3478"]}]},
             media_stream_constraints={
                 "video": True,
                 "audio": False
